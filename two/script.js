5aed6427b355f43493db059dedb82db89ae5d87e
RebeccaReis <111397529+RebeccaReis@users.noreply.github.com>
2022-10-15 14:17:39 -0300

    subindo session two

---
+++ two/script.js
@@ -0,0 +1,12 @@
+function interagindo(){
+ var resposta = prompt("por favor digite seu nome");
+
+ if(resposta != ''){
+    alert('Prazer Em Te Conhecer ' + resposta);
+ }
+ else{
+    alert('Você não escreveu nada :(');
+ }
+
+
+}
